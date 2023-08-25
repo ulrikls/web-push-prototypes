235fb0f215208e84fbc77061fab5f6bf47cd3c7e
Ulrik Taasti Landberg <ulrikls@gmail.com>
2023-08-25 15:49:46 +0200

    Initial app with working WebSocket impplementation

---
+++ build-logic/src/main/kotlin/delta.java-common-conventions.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    constraints {
+        implementation("io.javalin:javalin:5.6.1")
+        implementation("org.slf4j:slf4j-simple:2.0.7")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    }
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter("5.10.0")
+        }
+    }
+}
