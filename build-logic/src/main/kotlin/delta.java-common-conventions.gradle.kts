6df4680ff71315193286b38cb44a7b3284b5e1fa
Ulrik Taasti Landberg <ulrikls@gmail.com>
2023-09-04 11:04:38 +0200

    Upgrade Jackson

--- build-logic/src/main/kotlin/delta.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/delta.java-common-conventions.gradle.kts
@@ -10,8 +10,8 @@ dependencies {
     constraints {
         implementation("io.javalin:javalin:5.6.1")
         implementation("org.slf4j:slf4j-simple:2.0.7")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
-        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.15.1")
+        implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.1")
     }
 }
 
